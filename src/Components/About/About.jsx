cf6bfc04fa7a3f9d49910bc61e342560ae52e87c
Mohamed Ashraf <mohamed21-01148@student.eelu.edu.eg>
2025-10-28 18:23:26 +0300

    Enhance About and Home components with structured layout and styling

--- src/Components/About/About.jsx
+++ src/Components/About/About.jsx
@@ -1,9 +1,22 @@
-import React from 'react'
+import React from "react";
+import styles from "./About.module.css";
 
 export default function About() {
   return (
-    <div>
-      About
-    </div>
-  )
+      <section className={styles.aboutSection}>
+        <div className={styles.container}>
+          <h1 className={styles.title}>ABOUT COMPONENT</h1>
+          <div className={styles.divider}>
+            <div className={styles.line}></div>
+            <i className={`fa-solid fa-star ${styles.star}`}></i>
+            <div className={styles.line}></div>
+          </div>
+          <div className={styles.subtitle}>
+            <p>Freelancer is a free bootstrap theme created by Route. The download includes the complete source files including HTML, CSS, and JavaScript as well as optional<br /> SASS stylesheets for easy customization.</p>
+            <p>Freelancer is a free bootstrap theme created by Route. The download includes the complete source files including HTML, CSS, and JavaScript as well as optional<br /> SASS stylesheets for easy customization.</p>
+          </div>
+        </div>
+      </section>
+
+  );
 }
--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -1,9 +1,22 @@
-import React from 'react'
+import React from "react";
+import styles from "./Home.module.css";
+import avatar from "../../assets/img/home.svg"
 
 export default function Home() {
   return (
-    <div>
-        Home Page
-    </div>
-  )
+    <section className={styles.homeSection}>
+      <div className={styles.container}>
+        <img src={avatar} alt="avatar" className={styles.avatar} />
+        <h1 className={styles.title}>START FRAMEWORK</h1>
+        <div className={styles.divider}>
+          <div className={styles.line}></div>
+          <i className={`fa-solid fa-star ${styles.star}`}></i>
+          <div className={styles.line}></div>
+        </div>
+        <p className={styles.subtitle}>
+          Graphic Artist - Web Designer - Illustrator
+        </p>
+      </div>
+    </section>
+  );
 }
