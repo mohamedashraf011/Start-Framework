6efcbda7d5bb7b85e9d8edbb170a526daaac83f4
Mohamed Ashraf <mohamed21-01148@student.eelu.edu.eg>
2025-10-28 19:08:10 +0300

    Implement Contact component with form and styling; enhance About component layout

--- src/Components/About/About.jsx
+++ src/Components/About/About.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import styles from "./About.module.css";
 
+
 export default function About() {
   return (
       <section className={styles.aboutSection}>
--- src/Components/Contact/Contact.jsx
+++ src/Components/Contact/Contact.jsx
@@ -1,9 +1,44 @@
-import React from 'react'
+import React from "react";
+import styles from "./Contact.module.css";
+import { FaStar } from "react-icons/fa";
 
 export default function Contact() {
   return (
-    <div>
-      Contact
-    </div>
-  )
+    <section className={styles.contactSection}>
+      <div className={styles.container}>
+        <h2 className={styles.title}>CONTACT SECTION</h2>
+        <div className={styles.divider}>
+          <span className={styles.line}></span>
+          <FaStar className={styles.star} />
+          <span className={styles.line}></span>
+        </div>
+
+        <div className={styles.containerForm}>
+          <form className={styles.form}>
+            <div className={styles.inputGroup}>
+              <input type="text" id="userName" required />
+              <label htmlFor="userName">userName</label>
+            </div>
+
+            <div className={styles.inputGroup}>
+              <input type="number" id="userAge" required />
+              <label htmlFor="userAge">userAge</label>
+            </div>
+
+            <div className={styles.inputGroup}>
+              <input type="email" id="userEmail" required />
+              <label htmlFor="userEmail">userEmail</label>
+            </div>
+
+            <div className={styles.inputGroup}>
+              <input type="password" id="userPassword" required />
+              <label htmlFor="userPassword">userPassword</label>
+            </div>
+
+            <button className={styles.submitBtn}>send Message</button>
+          </form>
+        </div>
+      </div>
+    </section>
+  );
 }
